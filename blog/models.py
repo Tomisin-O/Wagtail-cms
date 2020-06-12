ecefc6a83826a488e92d09a7746cc919438fcc9e
Tomisin-O <onasanyatomisin@gmail.com>
2020-06-12 19:59:32 +0100

    Update models

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,18 @@ from wagtail.images.edit_handlers import ImageChooserPanel
     
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
+
     content_panels = Page.content_panels + [
-    FieldPanel('intro', classname="full")
+        FieldPanel('intro', classname="full")
 ]
+
+    def get_context(self, request):
+        # Update context to include only published posts,
+        # in reverse chronological order
+        context = super(BlogIndexPage, self).get_context(request)
+        live_blogpages = self.get_children().live()
+        context['blogpages'] = live_blogpages.order_by('-first_published_at')
+        return context
 
 class BlogPage(Page):
     date = models.DateField("Post date")
@@ -32,10 +41,4 @@ class BlogPage(Page):
 )
    
 
-    def get_context(self, request):
-        # Update context to include only published posts,
-        # in reverse chronological order
-        context = super(BlogIndexPage, self).get_context(request)
-        live_blogpages = self.get_children().live()
-        context['blogpages'] = live_blogpages.order_by('-first_published_at')
-        return context
+   
